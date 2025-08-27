2edefe3fa098fc77b5242a363bbd3150fb930dff
Fourt14n <hebertsep1914@gmail.com>
2025-08-27 19:53:46 -0300

    feat: Desenvolvendo melhor a responsividade

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -14,35 +14,35 @@ export default function Login() {
     }
 
     return (
-        <main className="flex flex-1">
-            <div className="logoContainer hidden md:flex flex-1 justify-center items-center h-screen">
-                <img src={loginLogo} alt="" />
+        <main className="flex flex-1 bg-(--bg-default)">
+            <div className="logoContainer hidden lg:flex flex-1 justify-center items-center h-screen bg-gradient-to-r from-[#00ff629f] to-(--bg-default)">
+                <img className="w-2/5" src={loginLogo} alt="" />
             </div>
-            <div className="loginContainer h-screen flex flex-2 justify-center items-center">
-                <Card className="flex w-11/12 justify-around">
+            <div className="loginContainer h-screen flex flex-2 justify-center items-center md:flex-2">
+                <Card className="flex justify-around md:w-2/3 md:py-20 lg:w-2/3 lg:py-24">
                     <CardHeader>
-                        <CardTitle className="font-display font-weight-regular text-lg flex justify-center">
+                        <CardTitle className="font-display font-weight-regular text-lg lg:text-3xl flex justify-center">
                             Faça seu login.
                         </CardTitle>
                     </CardHeader>
-                    <CardContent className="flex justify-items-start items-center w-full">
-                        <form className="w-full grid gap-6">
+                    <CardContent className="flex justify-items-start items-center w-full lg:justify-center">
+                        <form className="w-full grid gap-6 lg:w-4/5" >
                             <div className="flex flex-col gap-6">
                                 <div className="grid gap-2">
-                                    <Label className="font-display font-weight-regular text-lg" htmlFor="email">E-mail</Label>
+                                    <Label className="font-display font-weight-regular text-lg lg:text-xl" htmlFor="email">E-mail</Label>
                                     <Input
                                         id="email"
                                         type="email"
                                         placeholder="Digite aqui o seu e-mail"
-                                        className="text-sm"
+                                        className="text-sm lg:text-base"
                                         required
                                     />
                                 </div>
                                 <div className="grid gap-2">
                                     <div className="flex items-center">
-                                        <Label className="font-display font-weight-regular text-lg" htmlFor="password">Senha</Label>
+                                        <Label className="font-display font-weight-regular text-lg lg:text-xl" htmlFor="password">Senha</Label>
                                     </div>
-                                    <Input className="text-sm" placeholder="**********" id="password" type="password" required />
+                                    <Input className="text-sm lg:text-base" placeholder="**********" id="password" type="password" required />
                                     <a
                                         href="#"
                                         className="ml-auto inline-block text-sm underline-offset-4 underline hover:text-(--text-grey)"
@@ -53,14 +53,14 @@ export default function Login() {
                             </div>
                             <div className="flex flex-col gap-6">
                                 <div className="grid gap-2">
-                                    <Button className="bg-(--btn-default) text-(--text-strongGreen) hover:bg-(--btn-default-strong) cursor-pointer">Entrar</Button>
+                                    <Button className="bg-(--btn-default) text-(--text-strongGreen) hover:bg-(--btn-default-strong) cursor-pointer lg:text-base">Entrar</Button>
                                 </div>
                                 <div className="flex gap-2">
                                     <Checkbox id="rememberPassword" />
-                                    <Label htmlFor="rememberPassword">Lembrar de mim</Label>
+                                    <Label htmlFor="rememberPassword" className="lg:text-sm">Lembrar de mim</Label>
                                 </div>
                             </div>
-                            <span className="text-xs text-center">É novo aqui? Cote a melhor opção para a sua empresa <Link className="text-[#23D400]" to={"https://google.com"} >Aqui</Link></span>
+                            <span className="text-xs lg:text-sm text-center">É novo aqui? Cote a melhor opção para a sua empresa <Link className="text-[#23D400]" to={"https://google.com"} >Aqui</Link></span>
                         </form>
                     </CardContent>
                 </Card>
