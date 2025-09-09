9e85a6168ebf04ed1ca89d0c9d75401a218530c3
= <hebertsep1914@gmail.com>
2025-09-08 21:28:19 -0300

    feat: Completando e testando a lógica de popular dinamicamente

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,34 @@
 import HomeCard from "@/components/HomeCard/HomeCard";
 import { Clock } from "@/components/HomeClock/HomeClock";
 
+// Para fins educativos eu vou criar aqui um array do bagulho pra nós ver funcionando
+var equipes = [{
+    GroupName: "Meus Tickets",
+    GroupId: 1,
+    Total: 11,
+    OpenedToday: 7,
+    ExpiresToday: 2,
+    Expired: 2
+},
+{
+    GroupName: "Desenvolvimento",
+    GroupId: 2,
+    Total: 21,
+    OpenedToday: 7,
+    ExpiresToday: 2,
+    Expired: 12
+},
+{
+    GroupName: "TickDesk Sistemas LTDA",
+    GroupId: 3,
+    Total: 12,
+    OpenedToday: 4,
+    ExpiresToday: 5,
+    Expired: 3
+}
+
+]
+
 export default function Home(){
     return (
         <div className="w-full h-full px-5 md:px-20">
@@ -10,9 +38,7 @@ export default function Home(){
                 <Clock/>
             </div>
             <div className="flex flex-col gap-5 pb-10 lg:flex-row lg:pb:0 md-pb-5 lg:justify-center lg:items-center">
-                <HomeCard/>
-                <HomeCard/>
-                <HomeCard/>
+                {equipes.map(item => <HomeCard group={item}/>)}
             </div>
         </div>
     )
