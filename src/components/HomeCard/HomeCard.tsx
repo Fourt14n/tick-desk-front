c882201f574cdd8763920be707e1595d6a06827e
= <hebertsep1914@gmail.com>
2025-09-08 21:14:24 -0300

    feat: Desenvolvendo a tela de Home

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -14,39 +14,57 @@ interface ReceivingData {
     group: TicketGroup
 }
 
-export default function HomeCard(){
+export default function HomeCard() {
     return (
-        <Card className="max-w">
+        <Card className="lg:flex-1">
             <CardHeader>
-                <CardTitle className="text-center flex justify-center items-center">
-                    <p>Desenvolvimento</p>
-                    <Separator orientation="horizontal"/>
+                <CardTitle className="text-center flex justify-center items-center flex-col">
+                    <p className="cardTitle">Desenvolvimento</p>
+                    <Separator className="bg-[#BAB9B9] h-[1px] w-2/3 md:w-1/3 lg:w-2/10 mt-1" orientation="horizontal" />
                 </CardTitle>
             </CardHeader>
-            <CardContent>
-                <div className="w-full grid">
-                    <div className="topCards">
-                    <div className="ticketQtdContainer">
-                        <p>Total</p>
-                        <div className="quantityTextContainer">
-                            <p>19</p>
+            <CardContent className="px-3">
+                <div className="w-full max-w-full grid gap-2">
+                    <div className="topCards grid grid-cols-2 justify-between gap-3">
+                        <div className="ticketQtdContainer h-full aspect-[2-1] lg:aspect-square">
+                            <div className="flex w-full gap-3 flex-col justify-center items-center">
+                                <p className="text-center">Total</p>
+                                <div className="quantityTextContainer bg-[#1eff003b]">
+                                    <p className="font-bold">19</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ticketQtdContainer h-full aspect-[2-1] lg:aspect-square">
+                            <div className="flex w-full gap-3 flex-col justify-center items-center">
+                                <p className="text-center">Abertos hoje</p>
+                                <div className="quantityTextContainer bg-[#f6ff0065] text-center">
+                                    <p className="font-bold">19</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="bottomCards">
-                    <div className="ticketQtdContainer">
-                        <p>Total</p>
-                        <div className="quantityTextContainer">
-                            <p>19</p>
+                    <div className="bottomCards flex flex-col gap-2">
+                        <div className="ticketQtdContainer">
+                            <div className="flex w-3/5 gap-3 justify-center items-center">
+                                <p className="w-full">Vencem hoje</p>
+                            </div>
+                            <div className="flex w-2/5 gap-3 justify-center items-center">
+                                <div className="quantityTextContainer bg-[#ff80006b]">
+                                    <p className="font-bold">19</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ticketQtdContainer">
+                            <div className="flex w-3/5 gap-3 justify-center items-center">
+                                <p className="w-full">Vencidos</p>
+                            </div>
+                            <div className="flex w-2/5 gap-3 justify-center items-center">
+                                <div className="quantityTextContainer bg-[#ff08008a]">
+                                    <p className="font-bold">19</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="ticketQtdContainer">
-                        <p>Total</p>
-                        <div className="quantityTextContainer">
-                            <p>19</p>
-                        </div>
-                    </div>
-                </div>
                 </div>
             </CardContent>
         </Card>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,13 @@ import { Clock } from "@/components/HomeClock/HomeClock";
 
 export default function Home(){
     return (
-        <div className="w-full h-full px-8 md:px-14">
-            <div className="flex justify-center items-center md:justify-between w-full flex-col md:flex-row py-5 lg:py-20 gap-2 md:gap-0">
+        <div className="w-full h-full px-5 md:px-20">
+            <div className="flex justify-center items-center md:justify-between w-full flex-col md:flex-row py-5 lg:py-10 gap-2 md:gap-0">
                 <p className="text-(--grey) font-bold text-xl md:text-xl lg:text-2xl text-center">{`Seja bem-vindo(a), Hebert`}</p>
 
                 <Clock/>
             </div>
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5 pb-10 lg:flex-row lg:pb:0 md-pb-5 lg:justify-center lg:items-center">
                 <HomeCard/>
                 <HomeCard/>
                 <HomeCard/>
