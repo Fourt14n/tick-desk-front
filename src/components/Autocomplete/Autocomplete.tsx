4a7dfb6556c361d08f3c956223777dd53dd8b899
Fourt14n <hebertsep1914@gmail.com>
2025-10-20 20:26:44 -0300

    feat: Linkando a pesquisa dos tickets

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,41 +1,55 @@
 import { Input } from "../ui/input"
 import { useEffect, useRef, useState } from "react"
 import Searching from "../Searching/Searching"
+import { api } from "@/lib/axios"
+import { showError } from "@/hooks/useToast"
 
-const autoCompleteFake = [
-  {
-    callNumber: "123",
-    title: "Erro com integração com a api do Banco Inter"
-  },
-  {
-    callNumber: "124",
-    title: "Melhoria nos relatórios de recebimento"
-  },
-  {
-    callNumber: "125",
-    title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
-  },
-  {
-    callNumber: "125",
-    title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
-  },
-  {
-    callNumber: "125",
-    title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
-  },
-  {
-    callNumber: "125",
-    title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
-  },
-]
+
+type TicketResults = {
+  numberCall: string,
+  title: string
+}
+// const autoCompleteFake = [
+//   {
+//     callNumber: "123",
+//     title: "Erro com integração com a api do Banco Inter"
+//   },
+//   {
+//     callNumber: "124",
+//     title: "Melhoria nos relatórios de recebimento"
+//   },
+//   {
+//     callNumber: "125",
+//     title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
+//   },
+//   {
+//     callNumber: "125",
+//     title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
+//   },
+//   {
+//     callNumber: "125",
+//     title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
+//   },
+//   {
+//     callNumber: "125",
+//     title: "Lentidão no carregamento da tela de relatório de clientes Lentidão no carregamento daasdafasdfsdafdsfasdfsagdasd"
+//   },
+// ]
 
 
 export function AutoComplete() {
   const [search, setSearch] = useState<string>()
   const searchRef = useRef<HTMLDivElement>(null);
+  const [results, setResults] = useState<TicketResults[]>([]);
+  const timeoutRef = useRef<NodeJS.Timeout>(setTimeout(() => {})); // Timeout da busca
 
   function EraseSearch() {
     setSearch("");
+    setResults([]);
+  }
+
+  function BuscaChamado() {
+
   }
 
   useEffect(() => {
@@ -46,19 +60,34 @@ export function AutoComplete() {
     };
 
     document.addEventListener('mousedown', handleClickOutside);
-    
+
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+
+  }, [search]);
 
   return (
     <div className="relative">
-      <Input placeholder="Localize seu ticket" className="bg-white pr-8 w-full" value={search} onChange={(value) => setSearch(value.target.value)} />
+      <Input placeholder="Localize seu ticket" className="bg-white pr-8 w-full" value={search} onChange={(value) => {
+        setSearch(value.target.value)
+
+        clearTimeout(timeoutRef.current);
+
+        if (value.target.value) {
+          timeoutRef.current = setTimeout(() => {
+            api.get(`api/calls/search/?query=${value.target.value}`)
+              .then(res => setResults(res.data))
+              .catch(erro => showError(erro.response.data.error))
+          }, 1500)
+        }
+
+      }
+      } />
       {
         search && <div ref={searchRef} className="absolute w-full overflow-y-scroll max-h-56 bg-white z-20">
           {
-            autoCompleteFake.filter((f) => f.title.toUpperCase().includes(search.toUpperCase()) || f.callNumber.includes(search)).map((m) => {
+            results.filter((f) => f.title.toUpperCase().includes(search.toUpperCase()) || f.numberCall.includes(search)).map((m) => {
               return (
                 <Searching ticket={m} eraserFunc={EraseSearch} />
               )
--- src/components/Searching/Searching.tsx
+++ src/components/Searching/Searching.tsx
@@ -7,15 +7,15 @@ type Infos = {
 
 type TicketInfos = {
     title: string,
-    callNumber: string
+    numberCall: string
 }
 
 export default function Searching({ ticket, eraserFunc }: Infos) {
     return (
-        <Link to={`/Ticket/${ticket.callNumber}`} onClick={eraserFunc} className="flex flex-col border-1 border-black-50 gap-2 p-3 w-full">
+        <Link to={`/Ticket/${ticket.numberCall}`} onClick={eraserFunc} className="flex flex-col border-1 border-black-50 gap-2 p-3 w-full">
             <div>
                 <strong>
-                    {ticket.callNumber}
+                    {ticket.numberCall}
                 </strong>
             </div>
             <div className="truncate">
