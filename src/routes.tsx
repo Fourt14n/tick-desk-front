3608bc03296402bd9de83bb08c9951be831f8d24
= <hebertsep1914@gmail.com>
2025-10-25 18:24:21 -0300

    refactor: Refatorando o arquivo de rotas para ser mais legível

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,21 +11,24 @@ import { EAction } from "./types/EAction/EAction";
 import Team from "./pages/Team/Team";
 import RecoveryPassword from "./pages/RecoveryPassword/RecoveryPassword";
 
-export default function Router(){
+export default function Router() {
     return (
         <Routes>
-            <Route index element={<Login/>} path="/Login" />
-            <Route element={<RecoveryPassword/>} path="/RecuperacaoSenha" />
-            <Route path="/" element={<PrivateRoute children={<Layout/>}></PrivateRoute>}>
-                <Route path="Home/" index element={<PrivateRoute children={<Home/>}></PrivateRoute>}/>
-                <Route path="Ticket/:id" element={<PrivateRoute children={<Ticket/>}></PrivateRoute>}/>
-                <Route path="Listagem/:tipo" element={<PrivateRoute children={<Listagem/>}></PrivateRoute>}/>
-                <Route path="User/Create" element={<PrivateRoute children={<CreateUser action={EAction.CREATE}/>}></PrivateRoute>}/>
-                <Route path="User/:id" element={<PrivateRoute children={<CreateUser action={EAction.UPDATE}/>}></PrivateRoute>}/>
-                <Route path="Teams/Create" element={<PrivateRoute children={<Team action={EAction.CREATE}/>}></PrivateRoute>}/>
-                <Route path="Teams/:id" element={<PrivateRoute children={<Team action={EAction.UPDATE}/>}></PrivateRoute>}/>
+            <Route index element={<Login />} path="/Login" />
+            <Route element={<RecoveryPassword />} path="/RecuperacaoSenha" />
+            <Route element={<PrivateRoute children={<Layout/>} />}>
+                <Route path="/">
+                    <Route path="Home/" index element={<Home />} />
+                    <Route path="Ticket/:id" element={<Ticket />} />
+                    <Route path="Listagem/:tipo" element={<Listagem />} />
+                <Route path="User/Create" element={<CreateUser action={EAction.CREATE} />} />
+                <Route path="User/:id" element={<CreateUser action={EAction.UPDATE} />} />
+                <Route path="Teams/Create" element={<Team action={EAction.CREATE} /> } />
+                <Route path="Teams/:id" element={<Team action={EAction.UPDATE} /> } />
+                </Route>
             </Route>
-            <Route path="*" index element={<NotFound/>}/>
+            
+            <Route path="*" index element={<NotFound />} />
         </Routes>
     )
 }
